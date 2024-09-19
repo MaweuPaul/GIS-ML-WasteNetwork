4a400e5b761f5e2b2a905a3a062461be8ce6b4f3
MaweuPaul <maweupaul85@gmail.com>
2024-09-19 08:52:46 +0300

    update roadRoutes.js

--- BACKEND/Routes/roadRoutes.js
+++ BACKEND/Routes/roadRoutes.js
@@ -5,16 +5,28 @@ const {
   getRoad,
   getRoads,
   updateRoad,
-} = require('../Controllers/roadController.js');
-const multer = require('multer');
+  deleteAllRoads,
+} = require('../controllers/roadController');
 
 const router = express.Router();
-const upload = multer({ dest: 'uploads/' });
 
+// Middleware to parse JSON data
+router.use(express.json({ limit: '10mb' }));
+
+// Routes
 router.get('/', getRoads);
 router.get('/:id', getRoad);
-router.post('/', upload.single('file'), createRoad);
+router.post('/', createRoad);
 router.put('/:id', updateRoad);
 router.delete('/:id', deleteRoad);
+router.delete('/deleteAll', deleteAllRoads);
+
+// Error handling middleware
+router.use((error, req, res, next) => {
+  console.error('Router error:', error);
+  res
+    .status(500)
+    .json({ message: 'An unknown error occurred', error: error.message });
+});
 
 module.exports = router;
