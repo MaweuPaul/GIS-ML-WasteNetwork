7b7af2fcb42fa3537586071a84d4319739ceb37e
MaweuPaul <maweupaul85@gmail.com>
2024-09-19 08:51:04 +0300

    update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -5,9 +5,9 @@ const soilRoutes = require('./Routes/soilRoutes.js');
 const protectedAreaRoutes = require('./Routes/protectedAreasRoutes.js');
 const riverRoutes = require('./Routes/riverRoutes.js');
 const roadRoutes = require('./Routes/roadRoutes.js');
-const digitalElevationModelRoutes = require('./Routes/DemRoute.js');
-const geologyRoutes = require('./Routes/geologyRoute.js');
-const areaOfInterestRoutes = require('./Routes/aoiroute.js');
+// const digitalElevationModelRoutes = require('./Routes/demRoute.js');
+// const geologyRoutes = require('./Routes/geologyRoute.js');
+// const areaOfInterestRoutes = require('./Routes/aoiroute.js');
 
 dotenv.config();
 
@@ -17,12 +17,18 @@ const port = process.env.PORT || 3000;
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 
-app.use('/soils', soilRoutes);
-app.use('/protected-areas', protectedAreaRoutes);
-app.use('/rivers', riverRoutes);
-app.use('/roads', roadRoutes);
-app.use('/digital-elevation-models', digitalElevationModelRoutes);
-app.use('/geologies', geologyRoutes);
+app.use('/api/soils', soilRoutes);
+app.use('/api/protected-areas', protectedAreaRoutes);
+app.use('/api/rivers', riverRoutes);
+app.use('/api/roads', roadRoutes);
+// app.use('/api/digital-elevation-models', digitalElevationModelRoutes);
+// app.use('/api/geologies', geologyRoutes);
+// app.use('/api/area-of-interest', areaOfInterestRoutes);
+
+// Catch-all route for API
+app.use('/api/*', (req, res) => {
+  res.status(404).json({ message: 'API route not found' });
+});
 
 app.get('/', (req, res) => {
   res.send('Server is running');
