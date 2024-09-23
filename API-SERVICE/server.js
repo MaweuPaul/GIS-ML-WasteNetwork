23a6e8e193b7ec4fd0d14866b1fde85ac5c50b05
MaweuPaul <maweupaul85@gmail.com>
2024-09-23 10:51:17 +0300

    update server.js

--- API-SERVICE/server.js
+++ API-SERVICE/server.js
@@ -6,8 +6,8 @@ const protectedAreaRoutes = require('./Routes/protectedAreasRoutes.js');
 const riverRoutes = require('./Routes/riverRoutes.js');
 const roadRoutes = require('./Routes/roadRoutes.js');
 const digitalElevationModelRoutes = require('./Routes/demRoute.js');
-// const geologyRoutes = require('./Routes/geologyRoute.js');
 const areaOfInterestRoutes = require('./Routes/aoiroute.js');
+const databaseRoutes = require('./Routes/databaseRoutes.js');
 
 dotenv.config();
 
@@ -15,15 +15,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
-app.use(express.json());
+
+// Increase JSON payload limit to 50MB
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use('/api/soils', soilRoutes);
 app.use('/api/protected-areas', protectedAreaRoutes);
 app.use('/api/rivers', riverRoutes);
 app.use('/api/roads', roadRoutes);
 app.use('/api/digital-elevation-models', digitalElevationModelRoutes);
-// app.use('/api/geologies', geologyRoutes);
 app.use('/api/area-of-interest', areaOfInterestRoutes);
+app.use('/api/database', databaseRoutes);
 
 // Catch-all route for API
 app.use('/api/*', (req, res) => {
