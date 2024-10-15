c0eae34cae49ad2bdf6b6359586409bb0b2af4df
MaweuPaul <maweupaul85@gmail.com>
2024-10-15 23:34:44 +0300

    update resultsPage.jsx

--- FRONTEND/src/PAGES/resultsPage.jsx
+++ FRONTEND/src/PAGES/resultsPage.jsx
@@ -205,6 +205,14 @@ const ResultsPage = () => {
                       alt={`${key} Buffer`}
                       className="w-full h-auto rounded-md shadow-md"
                     />
+                    <a
+                      href={`${API_BASE_URL}/${path.replace(/\\/g, '/')}`}
+                      target="_blank"
+                      download={`${key}_buffer.png`}
+                      className="mt-2 inline-block px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                    >
+                      Download
+                    </a>
                   </div>
                 ))}
               </div>
