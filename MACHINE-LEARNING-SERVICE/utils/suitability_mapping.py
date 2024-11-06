1eb8f28307376c035f1ae51290b2539a1c2d9c17
MaweuPaul <maweupaul85@gmail.com>
2024-11-07 02:43:24 +0300

    update suitability_mapping.py

--- MACHINE-LEARNING-SERVICE/utils/suitability_mapping.py
+++ MACHINE-LEARNING-SERVICE/utils/suitability_mapping.py
@@ -1,102 +1,84 @@
+import numpy as np
+import pandas as pd
+
 def river_suitability_mapping(distance):
-    if distance <= 200:
+    if pd.isna(distance):
+        return np.nan
+    if distance <= 300:
         return 1  # Not suitable
-    elif 200 < distance <= 500:
+    elif 300 < distance <= 1000:
         return 2  # Less suitable
-    elif 500 < distance <= 1000:
-        return 3  # Moderately Suitable
     elif 1000 < distance <= 1500:
+        return 3  # Moderately suitable
+    elif 1500 < distance <= 2000:
         return 4  # Suitable
     else:
         return 5  # Highly suitable
-
-def residential_area_suitability_mapping(distance):
-    if distance <= 200:
-        return 1  # Not suitable
-    elif 200 < distance <= 500:
-        return 2  # Less suitable
-    elif 500 < distance <= 1000:
-        return 3  # Moderately Suitable
-    elif 1000 < distance <= 1500:
-        return 4  # Suitable
-    else:
-        return 5  # Highly suitable
-
-def soil_suitability_mapping(soil_type):
-    soil_type = soil_type.lower()
-    if soil_type == 'sand':
-        return 1  # Not suitable
-    elif soil_type == 'loam':
-        return 2  # Less suitable
-    elif soil_type == 'silt':
-        return 4  # Suitable
-    elif soil_type == 'clay':
-        return 5  # Highly suitable
-    else:
-        return 0  # Unknown soil type
 
 def road_suitability_mapping(distance):
-    if distance <= 200:
+    if pd.isna(distance):
+        return np.nan
+    if distance > 1200:
         return 1  # Not suitable
-    elif 200 < distance <= 500:
+    elif 1000 < distance <= 1200:
+        return 2  # Less suitable
+    elif 800 < distance <= 1000:
         return 3  # Moderately suitable
-    elif 500 < distance <= 1000:
-        return 5  # Highly Suitable
-    elif 1000 < distance <= 1500:
+    elif 400 < distance <= 800:
         return 4  # Suitable
     else:
-        return 2  # Less suitable
+        return 5  # Highly suitable
 
 def settlement_suitability_mapping(distance):
-    if distance <= 200:
+    if pd.isna(distance):
+        return np.nan
+    if distance <= 400:
         return 1  # Not suitable
-    elif 200 < distance <= 500:
+    elif 400 < distance <= 900:
         return 2  # Less suitable
-    elif 500 < distance <= 1000:
-        return 3  # Moderately Suitable
-    elif 1000 < distance <= 1500:
+    elif 1500 < distance <= 2100:
+        return 3  # Moderately suitable
+    elif distance > 2100:
         return 4  # Suitable
     else:
         return 5  # Highly suitable
 
-def protected_areas_suitability_mapping(distance):
-    if distance <= 200:
+def protectedarea_suitability_mapping(distance):
+    if pd.isna(distance):
+        return np.nan
+    if distance <= 300:
         return 1  # Not suitable
-    elif 200 < distance <= 500:
+    elif 300 < distance <= 1000:
         return 2  # Less suitable
-    elif 500 < distance <= 1000:
-        return 3  # Moderately Suitable
-    elif 1000 < distance <= 1500:
+    elif 1000 < distance <= 1500:  # Fixed the gap
+        return 3  # Moderately suitable
+    elif 1500 < distance <= 2000:
         return 4  # Suitable
     else:
         return 5  # Highly suitable
 
-def geology_suitability_mapping(geology_type):
-    # You haven't provided specific criteria for geology
-    # So I'm leaving this as a placeholder function
-    # You should implement the logic based on your geology classification
-    return 3  # Default to moderately suitable
-
-def landuse_suitability_mapping(land_use):
-    land_use = land_use.lower()
-    if land_use in ['built up', 'forests']:
-        return 1  # Not suitable
-    elif land_use == 'farmlands':
-        return 2  # Less suitable
-    elif land_use == 'bareland':
-        return 5  # Highly suitable
-    else:
-        return 0  # Unknown land use type
-
-# Additional function to map slope suitability
 def slope_suitability_mapping(slope_degree):
-    if slope_degree <= 5:
-        return 5  # Highly suitable (0-5 degrees)
-    elif 5 < slope_degree <= 10:
-        return 4  # Suitable (5-10 degrees)
-    elif 10 < slope_degree <= 15:
-        return 3  # Moderately suitable (10-15 degrees)
-    elif 15 < slope_degree <= 20:
-        return 2  # Less suitable (15-20 degrees)
+    if pd.isna(slope_degree):
+        return np.nan
+    if slope_degree > 18.6:
+        return 1  # Not suitable
+    elif 5.3 < slope_degree <= 18.6:
+        return 2  # Less suitable
+    elif 3.1 < slope_degree <= 5.2:
+        return 3  # Moderately suitable
+    elif 1.7 < slope_degree <= 3:
+        return 4  # Suitable
     else:
-        return 1  # Not suitable (>20 degrees)
+        return 5  # Highly suitable
+
+def land_use_suitability_mapping(land_use):
+    if pd.isna(land_use) or land_use is None:
+        return np.nan  # No data
+    if 'forests' in land_use or 'settlements' in land_use:
+        return 1  # Not suitable
+    elif 'farmlands' in land_use:
+        return 4  # Suitable
+    elif 'bareland' in land_use:
+        return 5  # Highly suitable
+    else:
+        return 2  # Less suitable
